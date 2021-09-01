7fed7597249a4c545363eb8add45839980c6fef6
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2021-09-01 16:55:15 +0200

    Add social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,32 @@
 import { Button } from 'react-bootstrap'
-import * as React from 'react' 
+import * as React from 'react'
 import xkrsymbol from '../images/xkrsymbol.png'
-
-//Add social with links etc
+import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
+import { FaTelegram } from "@react-icons/all-files/fa/FaTelegram";
+import { FaDiscord } from "@react-icons/all-files/fa/FaDiscord";
+import { FaReddit } from "@react-icons/all-files/fa/FaReddit";
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
+import { FaYoutube } from "@react-icons/all-files/fa/FaYoutube";
 
 const Footer = () => {
-    return(
+    return (
         <footer>
-            <img src={xkrsymbol} height='24' className='footersymbol' alt=''/>
+            <img src={xkrsymbol} height='24' className='footersymbol' alt='' />
             <div>
-            <a href='https://github.com/kryptokrona/kryptokrona-website.git'><Button variant='outline-light'><i></i>Edit Page</Button></a>
-            <p className='copyright'>©Kryptokrona 2021</p> 
+                <a href=''><Button variant='outline-light'><i></i>Edit Page</Button></a>
+                <p className='copyright'>©Kryptokrona 2021</p>
+                <a href='' aria-label="github" target="_blank" rel="noreferrer" id="footer-icon">
+                    <FaGithub /></a>
+                <a href='' aria-label="Discord" target="_blank" rel="noreferrer" id="footer-icon">
+                    <FaDiscord /></a>
+                <a href='' aria-label="telegram" target="_blank" rel="noreferrer" id="footer-icon">
+                    <FaTelegram /></a>
+                <a href='' aria-label="twitter" target="_blank" rel="noreferrer" id="footer-icon">
+                    <FaTwitter /></a>
+                <a href='' aria-label="reddit" target="_blank" rel="noreferrer" id="footer-icon">
+                    <FaReddit /></a>
+                <a href='' aria-label="youtube" target="_blank" rel="noreferrer" id="footer-icon">
+                    <FaYoutube /></a>
             </div>
         </footer>
     )
