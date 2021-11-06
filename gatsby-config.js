b6dccaa2af2a9bcdcbeaee31c357b8adde2adc4d
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-11-06 10:11:18 +0100

    removed - graphql could query

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,31 +37,6 @@ module.exports = {
       __key: "pages",
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/locales`,
-        name: `locale`
-      }
-    },
-    {
-      resolve: `gatsby-plugin-react-i18next`,
-      options: {
-        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
-        languages: [`en`, `se`, `no`],
-        defaultLanguage: `en`,
-        // if you are using Helmet, you must include siteUrl, and make sure you add http:https
-        siteUrl: `https://kryptokrona.org`,
-        // you can pass any i18next options
-        i18nextOptions: {
-          interpolation: {
-            escapeValue: false // not needed for react as it escapes by default
-          },
-          keySeparator: false,
-          nsSeparator: false
-        },
-      }
-    },
-    {
       resolve: "@chakra-ui/gatsby-plugin",
       options: {
         /**
