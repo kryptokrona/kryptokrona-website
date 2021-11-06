4148da8a492e5af51661e65dc64d5efb4815dbfe
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-11-06 11:06:04 +0100

    hmmm

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,8 +7,10 @@ import { useColorModeValue } from '@chakra-ui/color-mode'
 import AlertBanner from './AlertBanner'
 import xkrart from '../images/xkrart.svg'
 import { graphql } from 'gatsby';
+import {Trans, useTranslation} from 'gatsby-plugin-react-i18next';
 
 const Hero = () => {
+  const { t } = useTranslation();
     return (
     <Container maxW='7xl' overflow='hidden'>
         <AlertBanner/>
@@ -20,7 +22,7 @@ const Hero = () => {
             objectPosition='50% 50%'
             ></Img>
         <Stack pt='10' px={{base: 5, xl: 0, sm: 10,}} mt='5'>
-                <Heading align="start" fontSize={{base: '4xl', sm: '5xl', lg: '6xl', xl: '7xl'}} py='5'>A Nordic cryptocurrency for the future</Heading>
+                <Heading align="start" fontSize={{base: '4xl', sm: '5xl', lg: '6xl', xl: '7xl'}} py='5'><Trans>A Nordic cryptocurrency for the future</Trans></Heading>
                 <Text align="start" fontSize={{base: 'md', lg: 'lg', xl: 'xl'}}>On 2 April 2019, Kryptokrona was launched to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future.</Text>
             <Stack alignSelf="start" direction="row" pt='5'>
                 <Link to='/info'><Button
@@ -32,6 +34,7 @@ const Hero = () => {
                 }}
                 >Learn more</Button></Link>
                 <Link to='/download'><Button colorScheme="black" variant="outline" size="lg">Download</Button></Link>
+                <p>{t('message')}</p>
             </Stack>
         <Stack>
             
