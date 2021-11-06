84df24722c57244d2d34473eba3b4bff69b2a405
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-11-06 01:32:35 +0100

    Info text fix

--- src/pages/info.js
+++ src/pages/info.js
@@ -7,9 +7,9 @@ import StatusBar from '../components/StatusBar'
 const InfoPage = () => {
     return (
         <Layout>
-            <Container maxW='7xl' mb='10'>
-            <Heading mb='10' fontSize="7xl" align="start" marginTop="10">Money for the future.</Heading>
-            <Text fontSize="lg" align="start">Kryptokrona exists to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future.
+            <Container maxW='7xl' mb='10' p={{base: 5, xl: 0, sm: 10,}}>
+            <Heading mb='10' fontSize={{base: '4xl', sm: '5xl', lg: '6xl', xl: '7xl'}} align="start" marginTop="10">Money for the future.</Heading>
+            <Text fontSize={{base: 'md', lg: 'lg', xl: 'xl'}} align="start">Kryptokrona exists to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future.
 <br/><br/>Kryptokrona is a decentralized blockchain based on CryptoNote, which forms the basis for Monero, among others.
 <br/><br/>CryptoNote is a so-called “application layer” protocol further developed by TurtleCoin that enables things like: private transactions, messages and arbitrary data storage, completely decentralized.
 <br/><br/>On 2 April 2019, the platform was launched in part as a response to the Riksbank’s digital e-krona for the people’s right to create money and to safeguard the private economy without either commercial or state control.</Text>
