9faba84c343f68ee0973d54c4e578d89b17c708b
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-11-06 01:56:03 +0100

    React Helmet

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,10 +3,12 @@ import Footer from './Footer'
 import NavBar from './NavBar'
 
 import '../theme.scss'
+import { SEO } from './Seo'
 
 const Layout = (props) => {
     return (
         <div>
+            <SEO/>
             <NavBar/>
             {props.children}
             <Footer/>
