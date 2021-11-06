b31ce0a9d90df9ded22a528132244a0140521b45
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-11-06 11:38:58 +0100

    Revert "Added locales"
    This reverts commit e7d484be453dea7a8fe5df5cb0b07b2e2cf86218.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,8 @@ import { Img } from '@chakra-ui/react'
 import { useColorModeValue } from '@chakra-ui/color-mode'
 import AlertBanner from './AlertBanner'
 import xkrart from '../images/xkrart.svg'
-import {Trans, useTranslation} from 'gatsby-plugin-react-i18next';
-import {graphql} from 'gatsby';
 
 const Hero = () => {
-    const { t } = useTranslation();
     return (
     <Container maxW='7xl' overflow='hidden'>
         <AlertBanner/>
@@ -22,8 +19,8 @@ const Hero = () => {
             objectPosition='50% 50%'
             ></Img>
         <Stack pt='10' px={{base: 5, xl: 0, sm: 10,}} mt='5'>
-                <Heading align="start" fontSize={{base: '4xl', sm: '5xl', lg: '6xl', xl: '7xl'}} py='5'><Trans>A Nordic cryptocurrency for the future</Trans></Heading>
-                <Text align="start" fontSize={{base: 'md', lg: 'lg', xl: 'xl'}}><Trans>On 2 April 2019, Kryptokrona was launched to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future.</Trans></Text>
+                <Heading align="start" fontSize={{base: '4xl', sm: '5xl', lg: '6xl', xl: '7xl'}} py='5'>A Nordic cryptocurrency for the future</Heading>
+                <Text align="start" fontSize={{base: 'md', lg: 'lg', xl: 'xl'}}>On 2 April 2019, Kryptokrona was launched to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future.</Text>
             <Stack alignSelf="start" direction="row" pt='5'>
                 <GatsbyLink to='/info'><Button
                 bg={useColorModeValue('black', 'white')} 
@@ -32,8 +29,8 @@ const Hero = () => {
                 _hover={{
                     bg:'gray.500'
                 }}
-                ><Trans>Learn more</Trans></Button></GatsbyLink>
-                <GatsbyLink to='/download'><Button colorScheme="black" variant="outline" size="lg"><Trans>Download</Trans></Button></GatsbyLink>
+                >Learn more</Button></GatsbyLink>
+                <GatsbyLink to='/download'><Button colorScheme="black" variant="outline" size="lg">Download</Button></GatsbyLink>
             </Stack>
         <Stack>
             
@@ -44,17 +41,3 @@ const Hero = () => {
 }
 
 export default Hero
-
-export const query = graphql`
-  query($language: String!) {
-    locales: allLocale(filter: {language: {eq: $language}}) {
-      edges {
-        node {
-          ns
-          data
-          language
-        }
-      }
-    }
-  }
-`;
