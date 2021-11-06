2f26bb8a2cdc9439932545ef67bdff76bb6bc85e
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-11-06 02:03:11 +0100

    font

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -3,6 +3,10 @@ import { extendTheme } from "@chakra-ui/react"
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: true,
+  fonts: {
+    heading: "montserrat",
+    body: "roboto-mono",
+  },
 }
 
 const theme = extendTheme({ config })
