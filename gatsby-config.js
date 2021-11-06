e7d484be453dea7a8fe5df5cb0b07b2e2cf86218
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-11-06 09:28:56 +0100

    Added locales

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,40 @@ module.exports = {
       __key: "pages",
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locales`,
+        name: `locale`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`,
+        languages: [`en`, `fr`, `ar`],
+        defaultLanguage: `en`,
+        siteUrl: `http://localhost:8000/`,
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false 
+          },
+          keySeparator: false,
+          nsSeparator: false
+        },
+        pages: [
+          {
+            matchPath: '/:lang?/blog/:uid',
+            getLanguageFromPath: true,
+            excludeLanguages: ['es']
+          },
+          {
+            matchPath: '/preview',
+            languages: ['en']
+          }
+        ]
+      }
+    },
+    {
       resolve: "@chakra-ui/gatsby-plugin",
       options: {
         /**
