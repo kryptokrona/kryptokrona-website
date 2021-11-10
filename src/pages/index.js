eda46d683638c505aad87927d8c82c126f6f5e99
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-11-10 07:37:39 +0100

    Added CTA's to Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,17 @@ import Hero from "../components/Hero";
 import JoinCommunity from "../components/CTACommunity";
 import Layout from "../components/Layout";
 import StatusBar from "../components/StatusBar";
+import CTAhugin from "../components/CTAhugin";
+import CTAwallet from "../components/CTAwallet";
 
 const IndexPage = () => {
   return (
     <Layout>
       <Hero />
       <Features />
+      <CTAhugin/>
       <StatusBar />
+      <CTAwallet/>
       <JoinCommunity />
     </Layout>
   );
